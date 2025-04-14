4c97a25b67150805b62bad1e1132393734dbea61
Thomas Sansberro <thomas.sansberro@eemi.com>
2025-04-14 16:08:33 +0200

    Partie 2

--- public/index.php
+++ public/index.php
@@ -2,15 +2,18 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use app\controllers\SiteController;
+use app\controllers\AuthController;
 use app\core\Application;
 
 $app = new Application();
 
-$app->router->get("/", 'home');
+$app->router->get("/", [SiteController::class, 'home']);
+$app->router->get("/contact", [SiteController::class, 'contact']);
+$app->router->post("/contact", [SiteController::class, 'handleContact']);
 
-$app->router->get("/contact", 'contact');
-$app->router->post("/contact", function () {
-    return "Handling contact form submission";
-});
-
+$app->router->get("/login", [AuthController::class, 'login']);
+$app->router->post("/login", [AuthController::class, 'login']);
+$app->router->get("/register", [AuthController::class, 'register']);
+$app->router->post("/register", [AuthController::class, 'register']);
 $app->run();
